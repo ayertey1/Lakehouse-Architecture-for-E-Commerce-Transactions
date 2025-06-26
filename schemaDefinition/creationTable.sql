f89d8deda5831d28ab91137b5c370135f4d4d650
ayertey1 <peter.ayertey@amalitechtraining.org>
2025-06-26 19:58:00 +0000

    adding order items schema

--- schemaDefinition/creationTable.sql
+++ schemaDefinition/creationTable.sql
@@ -15,3 +15,16 @@ orders_schema = StructType([
     StructField("total_amount", DoubleType(), True),
     StructField("date", DateType(), False)
 ])
+
+--Order Items
+order_items_schema = StructType([
+    StructField("id", StringType(), False),
+    StructField("order_id", StringType(), False),
+    StructField("user_id", StringType(), False),
+    StructField("days_since_prior_order", IntegerType(), True),
+    StructField("product_id", StringType(), False),
+    StructField("add_to_cart_order", IntegerType(), True),
+    StructField("reordered", IntegerType(), True),
+    StructField("order_timestamp", TimestampType(), False),
+    StructField("date", DateType(), False)
+])

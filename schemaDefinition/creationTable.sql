6608a2b911cb2e69afec20d448fdf2a13ea77295
ayertey1 <peter.ayertey@amalitechtraining.org>
2025-06-26 19:55:07 +0000

    adding orders schema

--- schemaDefinition/creationTable.sql
+++ schemaDefinition/creationTable.sql
@@ -5,3 +5,13 @@ product_schema = StructType([
     StructField("department", StringType(), True),
     StructField("product_name", StringType(), True)
 ])
+
+--Orders
+orders_schema = StructType([
+    StructField("order_num", StringType(), False),
+    StructField("order_id", StringType(), False),
+    StructField("user_id", StringType(), False),
+    StructField("order_timestamp", TimestampType(), False),
+    StructField("total_amount", DoubleType(), True),
+    StructField("date", DateType(), False)
+])
